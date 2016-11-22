8d20ab536c830f25b87b3129b21873871ec6e1d5
Axel Niklasson <axel.niklasson@live.com>
2016-11-22 20:42:34 +0100

    Added some more tests

--- test/stops.js
+++ test/stops.js
@@ -2,6 +2,7 @@ var chai = require('chai');
 var expect = chai.expect;
 var nodeSkanetrafiken = require('../lib/skanetrafiken');
 
+/* findStop */
 describe('Find stop by name with correct parameters', function() {
     it('should successfully return stops matching the query', function(done) {
         nodeSkanetrafiken.findStop('Kristianstad', function(results, err) {
@@ -20,10 +21,59 @@ describe('Find stop by name with empty query', function() {
     });
 });
 
-describe('Find stop by name without', function() {
+/* findStartAndStop */
+describe('Find starts and stops by names with correct parameters', function() {
+    it('should successfully return starts and stops matching the query', function(done) {
+        nodeSkanetrafiken.findStartAndStop('Kristianstad', 'Lund', function(results, err) {
+            expect(results).to.not.be.empty;
+            done();
+        });
+    });
+});
+
+describe('Find stop by name with empty query', function() {
     it('should return null, since query was empty', function(done) {
-        nodeSkanetrafiken.findStop(function(results, err) {
+        nodeSkanetrafiken.findStartAndStop('', '', function(results, err) {
             expect(results).to.be.null;
+            done();
+        });
+    });
+});
+
+/* findNearestStops */
+describe('Find nearest stops by names with correct parameters', function() {
+    it('should successfully return nearest', function(done) {
+        nodeSkanetrafiken.findNearestStops(6167930, 1323215, 500, function(results, err) {
+            expect(results).to.not.be.empty;
+            done();
+        });
+    });
+});
+
+describe('Find nearest stops by names with correct parameters', function() {
+    it('should successfully return nearest stops', function(done) {
+        nodeSkanetrafiken.findNearestStops(6167930, 1323215, function(results, err) {
+            expect(results).to.not.be.empty;
+            done();
+        });
+    });
+});
+
+describe('Find stop by name with empty query', function() {
+    it('should return null, since query was empty', function(done) {
+        nodeSkanetrafiken.findStartAndStop(0, 0, function(results, err) {
+            expect(results).to.be.null;
+            done();
+        });
+    });
+});
+
+describe('Find nearest stops within specified radius', function() {
+    it('should successfully return nearest stops, all within specified radius', function(done) {
+        nodeSkanetrafiken.findNearestStops(6167930, 1323215, 500, function(results, err) {
+            for (var i = 0; i < results.length; i++) {
+                expect(results[i].Distance).to.be.most(500);
+            }
             done();
         });
     });
